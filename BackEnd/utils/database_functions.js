3d93580532c0abcf3fd6f4cb2fe3c3e017cf460a
Michaelsamir75 <michaelsamir142@gmail.com>
2023-07-09 14:45:59 +0200

    database_functions

--- BackEnd/utils/database_functions.js
+++ BackEnd/utils/database_functions.js
@@ -4,44 +4,167 @@ const Player = require('../models/player.module');
 const Board = require('../models/board.module');
 const Elements = require('../models/elements.module');
 
-function getUsernameById(userId) {
-  return User.findByPk(userId)
-    .then(user => {
-      if (!user) {
-        throw new Error(`User not found with ID ${userId}`);
-      }
-      return user.dataValues.Username;
-    })
-    .catch(error => {
-      console.error(error);
-      throw error;
-    });
+async function getUsernameById(userId) {
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      throw new Error(`User not found with ID ${userId}`);
+    }
+    return user.dataValues.Username;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
-function getNumberOfPlayers(RoomId) {
-  return Game.findByPk(RoomId)
-    .then(game => {
-      if (!game) {
-        throw new Error(`Game not found with ID ${RoomId}`);
-      }
-      return game.dataValues.NumberOfPlayers;
-    })
-    .catch(error => {
-      console.error(error);
-      throw error;
-    });
+async function getNumberOfPlayers(RoomId) {
+  try {
+    const game = await Game.findByPk(RoomId);
+    if (!game) {
+      throw new Error(`Game not found with ID ${RoomId}`);
+    }
+    return game.dataValues.NumberOfPlayers;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
+async function getCurrentNumberOfPlayers(RoomId) {
+  try {
+    const game = await Game.findByPk(RoomId);
+    if (!game) {
+      throw new Error(`Game not found with ID ${RoomId}`);
+    }
+    return game.dataValues.CurrentNoPlayers;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+// function for register
+async function createUser(username, password) {
+  try {
+    const user = await User.create({
+      Username: username,
+      Password: password
+    });
+    console.log("User created:", user);
+    return user;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+}
+// function for login
+async function checkCredentials(username, password) {
+  try {
+    const user = await User.findOne({ where: { Username: username, Password: password } });
+    return !!user; // Returns true if user exists, false otherwise
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+
+
+async function getPosition(userId, gameId) {
+  try {
+    const player = await Player.findOne({
+      where: { GameId: gameId, UserId: userId },
+      attributes: ['Position']
+    });
+
+    if (!player) {
+      throw new Error(`Player not found with Game ID ${gameId} and User ID ${userId}`);
+    }
+
+    return player.dataValues.Position;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+async function setGameStatus(RoomId, State) {
+  try {
+    const updatedRows = await Game.update(
+      { State: State },
+      { where: { RoomId: RoomId } }
+    );
+
+    if (updatedRows[0] === 0) {
+      throw new Error(`Game not found with ID ${gameId}`);
+    }
+
+    return State;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+async function getGameStatus(gameId) {
+  try {
+    const game = await Game.findByPk(gameId);
+    if (!game) {
+      throw new Error(`Game not found with ID ${gameId}`);
+    }
+    console.log(game)
+    return game.dataValues.State;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+// Example usage
+// const username = "JohnDoe";
+// const password = "secret";
+
+//createUser(username, password);
 
 // Example usage
 const userId = 1;
 const gameId = 2;
 
-getUsernameById(userId)
-  .then(username => {
-    console.log(username);
-  });
+// getPosition(userId, gameId)
+//   .then(position => {
+//     console.log(position);
+//   })
 
-getNumberOfPlayers(gameId)
-  .then(numberOfPlayers => {
-    console.log(numberOfPlayers);
-  });
+//setGameStatus(2, 'comted')
+  // .then(game => {
+  //   console.log("Game status set:", game.dataValues.State);
+  // })
+  // .catch(error => {
+  //   console.error(error);
+  // });
+
+// getGameStatus(gameId)
+//   .then(status => {
+//     console.log("Game status:", status);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+// (async () => {
+//   try {
+//     const username = await getUsernameById(userId);
+//     console.log(username);
+
+//     const numberOfPlayers = await getCurrentNumberOfPlayers(gameId);
+//     console.log(numberOfPlayers);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
+
+
+
+
+// checkCredentials(username, password)
+//   .then(result => {
+//     console.log(result); // true or false
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
