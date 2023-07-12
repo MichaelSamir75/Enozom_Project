3e5a83d4896c42b06d995d11d49c0c8cf13a42d4
MahmoudToubar <110629991+MahmoudToubar@users.noreply.github.com>
2023-07-13 01:52:52 +0300

    socket.io

---
+++ BackEnd/Socket.js
@@ -0,0 +1,45 @@
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = [];
+
+io.on("connection", (socket) => {
+  console.log(`players Connected: ${socket.id}`);
+
+   //notifies the server of the newly joined player
+
+  socket.on("join", (data) => {
+    users.join(data);
+    io.sockets.emit(`player with ID: ${users.id} joined: ${data}`);
+  });
+
+   // informs a new player who has just joined the game about the players who have already joined the game
+
+  socket.on("joined", (data) => {
+    socket.to(data.id).emit("joined", data);
+  });
+
+  socket.on("throwDice", (data) => {
+    users[data.id].pos = data.pos;
+    const turn = data.num != 6 ? (data.id + 1) % users.length : data.id;
+    io.sockets.emit("throwDice", data, turn);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("SERVER RUNNING");
+});
